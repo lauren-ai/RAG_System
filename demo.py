6eee99ae87c755b184d81b005588249d13156053
lauren-ai <3302727602@qq.com>
2025-11-04 22:05:05 +0800

    设置logger的信息，将print打印信息改为logger

--- demo.py
+++ demo.py
@@ -1,8 +1,9 @@
 from main import create_rag_system
 import time
+from utils import logger
 
 agent = create_rag_system()
-agent.process_document(r"D:\cyy\RAG_System\data\txt_test_sample.txt")
+agent.process_document(r"D:\RAG_System\data\txt_test_sample.txt")
 session_id = f"session_{int(time.time())}"
 config = {"configurable": {"thread_id": session_id}}
 
@@ -19,19 +20,18 @@ while True:
     has_source_in_answer = "（来源：" in answer
     
     search_results = result.get('search_results', [])
-    
-    print(f"\n Robot: {answer}")
+    logger.info(f"\n Robot: {answer}")
     
     if has_source_in_answer and search_results:
-        print(f"\n 引用原文段落: ")
+        logger.info(f"\n 引用原文段落: ")
         for i, result_item in enumerate(search_results, 1):
             text = result_item.get('text', '')
             score = result_item.get('score', 0)
             metadata = result_item.get('metadata', {})
             file_name = metadata.get('file_name')
             
-            print(f"\n({i}) 来源: {file_name} (相似度: {score:.4f})")
-            print(f"原文: {text[:200]}..." if len(text) > 200 else f"原文: {text}")
+            logger.info(f"\n({i}) 来源: {file_name} (相似度: {score:.4f})")
+            logger.info(f"原文: {text[:200]}..." if len(text) > 200 else f"原文: {text}")
     
     
 
